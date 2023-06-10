24eb9f9bf470c2e0dc98683923cc5aba8ead3116
Vanessa Nájera <vjndovale@gmail.com>
2023-06-10 14:08:35 -0500

    adiciones a main para poder ejecutar el juego, creacion controladores 1 y 2, modificaciones en ventanas 1 y 2

--- MVCJava/MVCJava/src/juegoMatematicasInfantiles123/controller/Controller2.java
+++ MVCJava/MVCJava/src/juegoMatematicasInfantiles123/controller/Controller2.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package juegoMatematicasInfantiles123.controller;
 
-/**
- *
- * @author Acer
- */
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class Controller2 {
+
+    @FXML
+    private Stage stage; // Usar el atributo stage
     
+    @FXML
+private Label label2;
+
+    @FXML
+    void showVentana3(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/juegoMatematicasInfantiles123/view/Ventana3.fxml"));
+        Parent root = loader.load();
+        Controller3 controller = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) label2.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+        this.stage.close();
+    }
+
+    public void setStage(Stage primaryStage) {
+        stage = primaryStage;
+    }
 }
+
